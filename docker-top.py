5b1fca69436799e6f7a15ab037a59fe9f1fb5896
slop <fest>
2026-05-14 20:11:01 +0300

    Project headers: yellow text on dark blue background
    256-color terminals use color 17 (very dark blue) background;
    16-color fallback uses standard blue. Yellow foreground improves
    readability against the dark background.

--- docker-top.py
+++ docker-top.py
@@ -144,7 +144,10 @@ class DockerTop:
         curses.init_pair(7, curses.COLOR_WHITE, curses.COLOR_GREEN)
         curses.init_pair(8, curses.COLOR_BLACK, curses.COLOR_WHITE)
         curses.init_pair(9, curses.COLOR_MAGENTA, -1)
-        curses.init_pair(10, curses.COLOR_WHITE, curses.COLOR_BLUE)
+        if curses.COLORS >= 256:
+            curses.init_pair(10, curses.COLOR_YELLOW, 17)
+        else:
+            curses.init_pair(10, curses.COLOR_YELLOW, curses.COLOR_BLUE)
 
         try:
             curses.curs_set(0)

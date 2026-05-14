2920970142f75ecfb096d18bb90cc307e73fdd8d
slop <fest>
2026-05-14 19:16:24 +0300

    Dark blue background for project headers
    Swap cyan background to dark blue (color code 44) for better readability.

--- docker-top.py
+++ docker-top.py
@@ -131,7 +131,7 @@ class DockerTop:
         curses.init_pair(7, curses.COLOR_WHITE, curses.COLOR_GREEN)
         curses.init_pair(8, curses.COLOR_BLACK, curses.COLOR_WHITE)
         curses.init_pair(9, curses.COLOR_MAGENTA, -1)
-        curses.init_pair(10, curses.COLOR_WHITE, curses.COLOR_CYAN)
+        curses.init_pair(10, curses.COLOR_WHITE, curses.COLOR_BLUE)
 
         try:
             curses.curs_set(0)
